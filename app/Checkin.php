975b40b433abcbe3e48791c999e56c195dfd0153
Niek <niek@bcome.nl>
2016-01-22 13:21:42 +0100

    Added calories calculation

--- app/Checkin.php
+++ app/Checkin.php
@@ -23,4 +23,33 @@ class Checkin extends Model
     protected $hidden = [
 
     ];
+
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $dates = [
+        'checkin', 'checkout'
+    ];
+
+    /**
+     * Returns the calories per minute for this checkin
+     *
+     * @param int $calories Calories per minute
+     * @return float Calories burned
+     */
+    public function burned($calories)
+    {
+        if (!$this->checkin || !$this->checkout) {
+            return 0.0;
+        }
+        $minDiff = $this->checkin->diffInMinutes($this->checkout);
+        $secDiff = $this->checkin->diffInSeconds($this->checkout) % 60;
+        $burned = $minDiff * $calories;
+        if ($secDiff > 0) {
+            $burned += ($secDiff / 60) * $calories;
+        }
+        return $burned;
+    }
 }
