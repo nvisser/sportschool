080d6bdc1d88cb806d7d454b183b849662eb0bcd
Niek <niek@bcome.nl>
2016-01-26 14:50:51 +0100

    Analytics

--- resources/views/layout/index.blade.php
+++ resources/views/layout/index.blade.php
@@ -14,6 +14,19 @@
     @include('_nav')
     @yield('content')
 </div>
+<script type="text/javascript">
+    var _paq = _paq || [];
+    _paq.push(['trackPageView']);
+    _paq.push(['enableLinkTracking']);
+    (function() {
+        var u="//stats.bcome.nl/";
+        _paq.push(['setTrackerUrl', u+'s.php']);
+        _paq.push(['setSiteId', 5]);
+        var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+        g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'s.js'; s.parentNode.insertBefore(g,s);
+    })();
+</script>
+<noscript><p><img src="//stats.bcome.nl/piwik.php?idsite=5" style="border:0;" alt="" /></p></noscript>
 <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
 <script src="<?= elixir('js/all.js') ?>"></script>
 @yield('scripts')
